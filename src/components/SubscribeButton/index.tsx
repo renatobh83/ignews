bf65c77159a7ac33f6652be75d1ce05deb9e3183
renatobh83 <neo_rlm@hotmail.com>
2021-09-21 13:00:24 -0300

    apiStripeFaundDB

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,10 +1,33 @@
 import styles from "./styles.module.scss";
+import { signIn, useSession } from "next-auth/client";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripejs";
 interface SubscribeButtonProps {
   priceId: string;
 }
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession();
+  async function handlerSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+    try {
+      const response = await api.post("/subscribe");
+      const { sessionId } = response.data;
+      const stripe = await getStripeJs();
+
+      stripe.redirectToCheckout({ sessionId: sessionId });
+    } catch (error) {
+      alert(error.message);
+    }
+  }
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handlerSubscribe}
+    >
       Subscribe now
     </button>
   );
