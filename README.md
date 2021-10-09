344d33ada351c752b8bda987375b8a707e82d8ea
renatobh83 <neo_rlm@hotmail.com>
2021-10-09 20:27:13 -0300

    readme

--- README.md
+++ README.md
@@ -1,34 +1,60 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Rockeseat Ignite Reactjs - Ig News
 
-## Getting Started
+<h1 align="center">
+  <img alt="Upfi" title="#Upfi" src="./public/images/logo.svg" />
+</h1>
 
-First, run the development server:
+[![TypeScript](https://img.shields.io/badge/--3178C6?logo=typescript&logoColor=ffffff)](https://www.typescriptlang.org/)
+[![JavaScript](https://img.shields.io/badge/--F7DF1E?logo=javascript&logoColor=000)](https://www.javascript.com/)
+
+### 🛠 Tecnologias
+
+- [Next-js](https://nextjs.org)
+- [TypeScript](https://www.typescriptlang.org/)
+- [Axios](https://github.com/axios/axios)
+
+#### 🛠 Frameworks
+
+- CMS - Prismic
+- Stripe
+- React-icons
+- Faunadb
+
+---
+
+<p align="center">
+Uma aplicacao blog aonde voce pode ter controle de acesso a leitura dos blogs a apenas assinantes do sites.
+</p>
+
+---
+
+<h4 align="center"> 
+	🚧  Ig News  🚀 Concluido.  🚧
+</h4>
+
+🔗 Install & Run
 
 ```bash
-npm run dev
-# or
-yarn dev
+ # clone repository
+  git clone https://github.com/renatobh83/ignews
+
+  # enter web folder
+  cd ignews
+
+  # download dependencies
+  yarn
+
+  # running / dev:
+  yarn dev
+
+  # create build
+  yarn build
+
+  # start
+  yarn start
+
+  # run tests
+  yarn test
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
-
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.js`.
-
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+[Live In](https://ignews-mytg9yx7e-renatobh83.vercel.app)
--- src/tests/pages/PostPreview.spec.tsx
+++ src/tests/pages/PostPreview.spec.tsx
@@ -1,79 +1,79 @@
 import { render, screen } from "@testing-library/react";
-import { getSession, useSession } from "next-auth/client";
+import { useSession } from "next-auth/client";
+import { useRouter } from "next/router";
 import { mocked } from "ts-jest/utils";
 import Post, { getStaticProps } from "../../pages/posts/preview/[slug]";
 import { getPrimiscClient } from "../../services/prismic";
 
-jest.mock("../../services/prismic");
-jest.mock("next-auth/client");
 const post = {
-  slug: "post",
-  title: "My post",
-  content: "<p>My post excertp</p>",
-  updatedAt: "10 de abril",
+  slug: "my-new-post",
+  title: "My New Post",
+  content: "<p>Post excerpt</p>",
+  updatedAt: "10 de Abril",
 };
-describe("Posts page", () => {
+
+jest.mock("next-auth/client");
+jest.mock("next/router");
+jest.mock("../../services/prismic");
+
+describe("Post preview page", () => {
   it("renders correctly", () => {
     const useSessionMocked = mocked(useSession);
+
     useSessionMocked.mockReturnValueOnce([null, false]);
+
     render(<Post post={post} />);
-    expect(screen.getByText("My post")).toBeInTheDocument();
-    expect(screen.getByText("My post excertp")).toBeInTheDocument();
+
+    expect(screen.getByText("My New Post")).toBeInTheDocument();
+    expect(screen.getByText("Post excerpt")).toBeInTheDocument();
+    expect(screen.getByText("Wanna continue reading?")).toBeInTheDocument();
   });
 
-  //   it("redirects users if no subscription is found", async () => {
-  //     const getSessionsMocked = mocked(getSession);
+  it("redirects user to full post when user is subscribed", async () => {
+    const useSessionMocked = mocked(useSession);
+    const useRouterMocked = mocked(useRouter);
+    const pushMock = jest.fn();
 
-  //     getSessionsMocked.mockResolvedValueOnce({
-  //       activeSubscription: null,
-  //     });
-  //     const response = await getServerSideProps({
-  //       params: {
-  //         slug: "post",
-  //       },
-  //     } as any);
+    useSessionMocked.mockReturnValueOnce([
+      { activeSubscription: "fake-active-subscription" },
+      false,
+    ] as any);
 
-  //     expect(response).toEqual(
-  //       expect.objectContaining({
-  //         redirect: expect.objectContaining({
-  //           destination: "/posts/preview/post",
-  //         }),
-  //       })
-  //     );
-  //   });
-  //   it("loads initial data", async () => {
-  //     const getSessionsMocked = mocked(getSession);
-  //     const getPrimiscClientMocked = mocked(getPrimiscClient);
+    useRouterMocked.mockReturnValueOnce({
+      push: pushMock,
+    } as any);
 
-  //     getPrimiscClientMocked.mockReturnValue({
-  //       getByUID: jest.fn().mockResolvedValueOnce({
-  //         data: {
-  //           title: [{ type: "heading", text: "My post" }],
-  //           content: [{ type: "paragraph", text: "My post excertp" }],
-  //         },
-  //         last_publication_date: "04-01-2021",
-  //       }),
-  //     } as any);
-  //     getSessionsMocked.mockResolvedValueOnce({
-  //       activeSubscription: "fake-active-sb",
-  //     } as any);
-  //     const response = await getServerSideProps({
-  //       params: {
-  //         slug: "post",
-  //       },
-  //     } as any);
+    render(<Post post={post} />);
 
-  //     expect(response).toEqual(
-  //       expect.objectContaining({
-  //         props: {
-  //           post: {
-  //             slug: "post",
-  //             title: "My post",
-  //             content: "<p>My post excertp</p>",
-  //             updatedAt: "01 de abril de 2021",
-  //           },
-  //         },
-  //       })
-  //     );
-  //   });
+    expect(pushMock).toHaveBeenCalledWith("/posts/my-new-post");
+  });
+
+  it("loads initial data", async () => {
+    const getPrismicClientMocked = mocked(getPrimiscClient);
+
+    getPrismicClientMocked.mockReturnValueOnce({
+      getByUID: jest.fn().mockResolvedValueOnce({
+        data: {
+          title: [{ type: "heading", text: "My new post" }],
+          content: [{ type: "paragraph", text: "Post content" }],
+        },
+        last_publication_date: "04-01-2021",
+      }),
+    } as any);
+
+    const response = await getStaticProps({ params: { slug: "my-new-post" } });
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          post: {
+            slug: "my-new-post",
+            title: "My new post",
+            content: "<p>Post content</p>",
+            updatedAt: "01 de abril de 2021",
+          },
+        },
+      })
+    );
+  });
 });
